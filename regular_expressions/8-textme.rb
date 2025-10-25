7d488aaee5eef5978fae6e96927eeffcbea84618
OrpheliePerrine <m.perrine@alustudent.com>
2025-10-25 13:13:57 -0700

    repetition

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 log_line = ARGV[0]
-
 if log_line =~ /\[from:(?<sender>[^\]]+)\].*\[to:(?<receiver>[^\]]+)\].*\[flags:(?<flags>[^\]]+)\]/
   puts "#{Regexp.last_match[:sender]},#{Regexp.last_match[:receiver]},#{Regexp.last_match[:flags]}"
 end
+
