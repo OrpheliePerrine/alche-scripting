ed1d839b1769c9cfb30c6ef6cea15a4aee33a2d9
OrpheliePerrine <m.perrine@alustudent.com>
2025-10-26 11:13:53 -0700

    text me

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-log_line = ARGV[0]
-if log_line =~ /\[from:(?<sender>[^\]]+)\].*\[to:(?<receiver>[^\]]+)\].*\[flags:(?<flags>[^\]]+)\]/
-  puts "#{Regexp.last_match[:sender]},#{Regexp.last_match[:receiver]},#{Regexp.last_match[:flags]}"
-end
-
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).map { |a| a.join(",") }
